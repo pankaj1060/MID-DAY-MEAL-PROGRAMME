c7307f0ab80e48042564993ee9283e720081539c
pankaj <pankajkumar1060@gmail.com>
2020-05-22 06:20:41 +0530

    make it better

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const bodyparser = require('body-parser');
-const request = require("request");
-const eventemitter = require('events');
 var PORT = process.env.PORT || 5000;
 const app = express();
-const emitter = new eventemitter();
 
-emitter.on("click", function() {
-
-})
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 
